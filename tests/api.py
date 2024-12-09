ad246a060ab2df82d63358cc86e1d4e61e6635b7
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-08 17:00:15 -0800

    feat: added some user creation tests, updated docs

--- tests/api.py
+++ tests/api.py
@@ -8,4 +8,31 @@ def test_404():
     assert response.status_code == 404
     assert "error" in response.json()
 
+# Testing user creation
+def test_create_user():
+    url = "http://localhost:8000/users"
+    data = {
+        "name": "John Doe",
+        "email": "johndoe@ansengarvin.com",
+        "password": "password",
+        "weight": 150
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 201
+    assert "id" in response.json()
+    assert response.json()["name"] == data["name"]
+    assert response.json()["email"] == data["email"]
+    assert response.json()["weight"] == data["weight"]
+
+# Testing creation of a duplicate user
+def test_duplicate_user():
+    url = "http://localhost:8000/users"
+    data = {
+        "name": "John Doe",
+        "email": "johndoe@ansengarvin.com",
+        "password": "password",
+        "weight": 150
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 403
 
