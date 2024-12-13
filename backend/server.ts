c5febac84ffa5c34427ac94bc3be8668b0fd3f6e
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-12 17:34:58 -0800

    chore: removed obsolete api call

--- backend/server.ts
+++ backend/server.ts
@@ -28,42 +28,6 @@ app.get('/test', async function (req, res) {
     
 });
 
-app.post('/users/:id/days/:date/foods', requireAuthentication, async function(req, res) {
-    try {
-        if (! req.body.calories) {
-            res.status(400).send({
-                error: "Request body must contain calories"
-            })
-        } else {
-            // TODO: If no position provided, calculate position based on the positions of other foods and recipes on this day.
-
-            // TODO: If position provided, insert into position and push all other positions down. Will need to use a transaction to do this in batch.
-
-            // Insert the food into the database
-            const text = 'INSERT INTO foods(date, user_id, name, calories, position) VALUES($1, $2, $3, $4, $5) RETURNING id'
-            const values = [req.params.date, req.params.id, req.body.id, req.body.calories, 0]
-
-            const result = await pool.query(text, values)
-
-            if (!result.rowCount) {
-                res.status(400).send({
-                    error: "Error inserting food"
-                })
-            } else {
-                res.status(201).send({
-                    id: result.rows[0].id
-                })
-            }
-        }
-
-    } catch (err) {
-        res.status(400).send({
-            err: err.message
-        })
-    }
-    
-})
-
 app.use('*', function (req, res) {
     res.status(404).json({ error: "Requested resource '" + req.originalUrl + "' Not Found" });
 })
