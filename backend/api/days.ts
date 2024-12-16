1fd4678cd1f7cfafd608739e511a88d917417a55
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-16 15:50:04 -0800

    feat: function to check if day exists or otherwise create

--- backend/api/days.ts
+++ backend/api/days.ts
@@ -4,11 +4,26 @@ import {pool} from '../lib/database'
 
 const router = Router()
 
+async function check_if_day_exists_or_create(user, date: String) {
+    let text = "SELECT id FROM days WHERE date = $1"
+    let values = [date]
+    let result = await pool.query(text, values)
+
+    if (result.rowCount != 0) {
+        return result.rows[0].id
+    } else {
+        text = "INSERT INTO days(user_id, date) VALUES($1, $2) RETURNING id"
+        values = [user, date]
+        result = await pool.query(text, values)
+        return result.rows[0].id
+    }
+}
+
 // Make a new day
 router.post('/', requireAuthentication, async function(req, res) {
     try {
         // Ensure user_id and date provided
-        if (!req.body.date) {
+        if (! req.body || !req.body.date) {
             res.status(400).send({err: "Missing date in request body"})
 
         } else {
@@ -42,7 +57,24 @@ router.post('/', requireAuthentication, async function(req, res) {
     } catch( err ) {
         console.log(err)
         res.status(500).send({
-            error: "Error creating day."
+            error: "server error"
+        })
+    }
+})
+
+// Add a food to a day
+router.post('/days/:date/food', requireAuthentication, async function(req, res) {
+    try {
+        if (!req.body || !req.body.calories) {
+            res.status(400).send({err: "entry must have request body with calories"})
+        } else {
+            // Gets the user's day ID
+            let day_id = check_if_day_exists_or_create(req.user, req.params.date)
+        }
+    } catch (err) { 
+        console.log(err)
+        res.status(500).send({
+            err: "server error"
         })
     }
 })
