4184c9a9e1478d41fc679255cf4e2f6a141b7b09
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-17 19:47:30 -0800

    feat: added redirects to login, but i think i need to make it a modal instead

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,7 +1,7 @@
 function Login() {
     return (
       <>
-        Profile
+        Login
       </>
     )
   }
--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,4 +1,16 @@
+import Cookies from 'js-cookie'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 function Profile() {
+    let navigate = useNavigate()
+
+    useEffect(() => {
+        if (!Cookies.get("auth")) {
+          navigate('/login')
+        }
+    })
+
     return (
       <>
         Profile
