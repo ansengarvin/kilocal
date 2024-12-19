6e8d963e62260c255f3e0ed2a8c27ff172ce74ee
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-18 21:32:09 -0800

    fix: all queries caches are now cleared on logout (repeat data was being displayed on login modal when username and password was entered w/o pressing login)

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,14 +1,17 @@
+import { useQueryClient } from "@tanstack/react-query"
 import Cookies from "js-cookie"
 import { useNavigate, useOutletContext } from "react-router-dom"
 
 function Profile() {
   const {setLoggedIn} = useOutletContext<{setLoggedIn: Function}>()
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
     return (
       <>
         <button onClick={(e) => {
           e.preventDefault
           Cookies.remove("auth")
+          queryClient.clear()
           setLoggedIn(false)
           navigate('/')
         }}>
