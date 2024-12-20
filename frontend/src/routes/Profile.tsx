1f31de67f39fb24159926807b2c0165b88a0ac70
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-19 20:41:19 -0800

    feat: profile gets user info from api

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,4 +1,4 @@
-import { useQueryClient } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import Cookies from "js-cookie"
 import { useNavigate, useOutletContext } from "react-router-dom"
 import { ContentWindow } from "../components/ContentWindow"
@@ -15,9 +15,37 @@ function Profile() {
   const navigate = useNavigate()
   const queryClient = useQueryClient()
 
+  const {isLoading, error, data} = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const url = "http://localhost:8000/users/"
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          'Authorization': 'Bearer ' + Cookies.get("auth")
+        } 
+      })
+      return response.json()
+    }
+  })
+
   return (
     <ContentWindow>
       <div className="content">
+        {isLoading ? <>Loading</> : <></>}
+        {error ? <>Error</> : <></>}
+        {data &&
+          <>
+          <h1>
+          {data.name}'s Profile
+          </h1>
+          <p>
+            Email: {data.email}<br/>
+            Weight: {data.weight}
+          </p>
+            
+          </>
+        }
         <SignOutButton onClick={(e) => {
           e.preventDefault
           Cookies.remove("auth")
