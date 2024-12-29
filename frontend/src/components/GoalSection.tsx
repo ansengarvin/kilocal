657532e97fa13bdfe595c4de6fdae06e1fa2c544
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-12-28 18:19:37 -0800

    feat: moved progress bar into separate reuseable component, added props to control appearance

--- frontend/src/components/GoalSection.tsx
+++ frontend/src/components/GoalSection.tsx
@@ -1,46 +1,12 @@
 import styled from "@emotion/styled";
+import { ProgressBar } from "./ProgressBar";
 
 interface GoalSectionProps {
     calorieTotal: number,
     calorieGoal: number,
 }
 
-interface progressBarProps{
-    width: string
-}
 
-const TotalCalDiv = styled.div<progressBarProps>`
-  height: 30px;
-  width: 90%;
-  position: relative;
-
-  display: flex;
-  gap: 10px;
-  justify-content: center;
-
-  font-size: 24px;
-
-  background-color: white;
-  border-radius: 25px;
-  
-  .number {
-    position: absolute;
-    color: #343434;
-  }
-
-  .progressBar {
-    width: ${props => props.width};
-    height: inherit;
-    background-color: #c3c3c3;
-    border-radius: 25px;
-    margin-right: auto;
-  }
-`
-
-interface totalCalProps {
-    total: number
-    goal: number
-}
 
 const GoalSectionDiv = styled.div`
     background-color: #adadad;
@@ -53,31 +19,19 @@ const GoalSectionDiv = styled.div`
     justify-content: center;
     align-items: center;
 `
-function TotalCal(props: totalCalProps) {
-    const {total, goal} = props
 
-    const progressWidth = (`${(total / 2000 * 100).toString()}%`)
-    console.log(progressWidth)
-
-    const labelText = `${total} out of 2000."`
-
-    return (
-        <TotalCalDiv width={progressWidth}>
-        <div className="progressBar">
-
-        </div>
-        <div className="number total" tabIndex={0} aria-label={labelText}>
-            {total} / {goal}
-        </div>
-        </TotalCalDiv>
-    )
-}
 
 export function GoalSection(props: GoalSectionProps) {
     const {calorieTotal, calorieGoal} = props
     return (
         <GoalSectionDiv>
-            <TotalCal total={calorieTotal} goal={calorieGoal}/>
+            <ProgressBar
+                value={calorieTotal}
+                goal={calorieGoal}
+                height="30px"
+                width="90%"
+                fontSize="24px"
+            />
         </GoalSectionDiv>
     )
 }
---
+++ frontend/src/components/ProgressBar.tsx
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled"
+
+interface progressBarDivProps{
+    barWidth: string
+    barHeight: string
+    progressWidth: string
+    fontSize: string
+}
+
+const ProgressBarDiv = styled.div<progressBarDivProps>`
+  height: ${props => props.barHeight};
+  width: ${props => props.barWidth};
+  position: relative;
+
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+
+  font-size: ${props => props.fontSize};
+
+  background-color: white;
+  border-radius: 25px;
+  
+  .number {
+    position: absolute;
+    color: #343434;
+  }
+
+  .progressBar {
+    width: ${props => props.progressWidth};
+    height: inherit;
+    background-color: #c3c3c3;
+    border-radius: 25px;
+    margin-right: auto;
+  }
+`
+
+interface progressBarProps {
+    value: number
+    goal: number
+    height: string
+    width: string
+    fontSize: string
+}
+
+export function ProgressBar(props: progressBarProps) {
+    const {value, goal, height, width, fontSize} = props
+
+    const progressWidth = (`${(value / 2000 * 100).toString()}%`)
+    console.log(progressWidth)
+
+    const labelText = `${value} out of 2000."`
+
+    return (
+        <ProgressBarDiv 
+            barWidth =  {width}
+            barHeight = {height}
+            progressWidth = {progressWidth}
+            fontSize={fontSize}
+        >
+            <div className="progressBar"/>
+            <div className="number total" tabIndex={0} aria-label={labelText}>
+                {value} / {goal}
+            </div>
+        </ProgressBarDiv>
+    )
+}
