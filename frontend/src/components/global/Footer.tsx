8556533ee387d2c8f9ea43f6a81eb2a589887040
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-01-01 20:35:13 -0800

    feat: added footer

---
+++ frontend/src/components/global/Footer.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const FooterStyle = styled.div`
+    grid-area: footer;
+    height: 0px;
+    margin-top: 10px;
+`
+
+export function Footer() {
+    return(
+        <FooterStyle/>
+    )
+}
