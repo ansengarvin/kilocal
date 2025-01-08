798d75bc248845611b621613bf2d37617d2fbc5a
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-01-08 14:06:16 -0800

    fix: added -y flags to each apt-get install in script, attempting to fix docker install problem with apt-get update

--- backend/init.sh
+++ backend/init.sh
@@ -1,7 +1,7 @@
 function install_npm() {
     sudo apt-get update
 
-    sudo apt-get install npm
+    sudo apt-get install -y npm
 
     npm install
 }
@@ -9,7 +9,7 @@ function install_npm() {
 function get_docker() {
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,8 +20,11 @@ function get_docker() {
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+    # Update package
+    sudo apt-get update
+
     # Install docker
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Verify that the installation is successful by running the hello-world image
     sudo docker run hello-world
@@ -32,7 +35,7 @@ function get_nginx() {
     sudo apt-get update
 
     # Install nginx
-    sudo apt-get install nginx
+    sudo apt-get install -y nginx
 
     # Verify the installation
     sudo nginx -v
