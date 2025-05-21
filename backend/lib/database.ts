eaf8d21a1c0483318aa03151301542ed06ec81c4
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-20 22:49:12 -0700

    fix: azure setup requires db name

--- backend/lib/database.ts
+++ backend/lib/database.ts
@@ -6,15 +6,13 @@ console.log(process.env.DB_PASS);
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_PORT);
 console.log(process.env.DB_NAME);
-console.log(process.env.CORS_URL);
-console.log(process.env.DB_SSL);
-console.log(process.env.CA_PATH);
 
 const sqlConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     server: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
     options: {
         encrypt: true,
         trustServerCertificate: true,
