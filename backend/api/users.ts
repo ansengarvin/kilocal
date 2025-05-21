ddb3dbc844d64ee547f2d84dcaf7bc2bd9f184d4
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-20 19:22:50 -0700

    refactor: user api endpoints use mssql syntax

--- backend/api/users.ts
+++ backend/api/users.ts
@@ -32,27 +32,33 @@ router.post("/login", requireAuthentication, async function (req, res) {
     }
 });
 
-/*
 router.delete("/:user_id", requireAuthentication, async function (req, res) {
     try {
-        const text = "DELETE FROM users WHERE id = $1";
-        const values = [req.params.user_id];
-        await pool.query(text, values);
-        res.status(204).send();
+        const pool = await getPool();
+        const userId = req.params.user_id;
+        const result = await pool.request().input("id", userId).query("DELETE FROM users WHERE id = @id");
+        if (result.rowsAffected[0] > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).send({ err: "User not found" });
+        }
     } catch (err) {
         res.status(400).send({
-            err: err,
+            err: err.message,
         });
     }
 });
 
 router.get("/", requireAuthentication, async function (req, res) {
     try {
-        const text = "SELECT id, name, email, weight FROM users WHERE id = $1";
-        const values = [req.user];
-        const result = await pool.query(text, values);
-        if (result.rowCount) {
-            res.status(200).send(result.rows[0]);
+        const pool = await getPool();
+        const uid = req.user;
+        const result = await pool
+            .request()
+            .input("id", uid)
+            .query("SELECT id, name, email, weight FROM users WHERE id = @id");
+        if (result.recordset.length) {
+            res.status(200).send(result.recordset[0]);
         } else {
             res.status(404).send({
                 err: "User not found",
@@ -64,6 +70,5 @@ router.get("/", requireAuthentication, async function (req, res) {
         });
     }
 });
-*/
 
 module.exports = router;
