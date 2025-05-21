92c3b761861a5e21b45de4d967d4f25f85c8955e
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-20 20:02:42 -0700

    chore: removed disgusting python test code

--- tests/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-
-def test_404():
-    url = "http://localhost:8000/does_not_exist"
-    response = requests.get(url)
-    assert response.status_code == 404
-    assert "error" in response.json()
