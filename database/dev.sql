a2a1afd2ec54ad6315839dca9a77a0448ab69a07
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-20 19:01:23 -0700

    wip: local dev environment accepts sql file

--- database/dev.sql
+++ database/dev.sql
@@ -5,13 +5,6 @@ CREATE TABLE Users (
     weight FLOAT
 );
 
-CREATE TABLE Recipes (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    user_id NVARCHAR(255) NOT NULL,
-    name NVARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
-);
-
 CREATE TABLE Days (
     id INT IDENTITY(1,1) PRIMARY KEY,
     user_id NVARCHAR(255) NOT NULL,
@@ -34,46 +27,19 @@ CREATE TABLE Foods (
         (day_id IS NOT NULL AND recipe_id IS NULL) OR
         (day_id IS NULL AND recipe_id IS NOT NULL)
     ),
-    FOREIGN KEY (day_id) REFERENCES Days(id) ON DELETE CASCADE,
-    FOREIGN KEY (recipe_id) REFERENCES Recipes(id) ON DELETE CASCADE
-);
-
-CREATE TABLE Days_Recipes (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    date DATE NOT NULL,
-    recipe_id INT NOT NULL,
-    position INT NOT NULL,
-    FOREIGN KEY (recipe_id) REFERENCES Recipes(id) ON DELETE CASCADE
+    FOREIGN KEY (day_id) REFERENCES Days(id) ON DELETE CASCADE
 );
 
 -- Insert test users
 INSERT INTO Users (id, email, name, weight) VALUES
-('user1', 'ansengarvin@gmail.com', 'Ansen', 65.5),
-
--- Insert test recipes
-INSERT INTO Recipes (user_id, name) VALUES
-('user1', 'Chicken Salad'),
-('user2', 'Oatmeal Bowl');
+('user1', 'ansengarvin@gmail.com', 'Ansen', 65.5);
 
 -- Insert test days
 INSERT INTO Days (user_id, date) VALUES
-('user1', '2025-05-20'),
-('user2', '2025-05-20');
+('user1', '2025-05-20');
 
 -- Insert test foods (linked to a day)
 INSERT INTO Foods (day_id, name, calories, amount, carbs, fat, protein, position)
 VALUES
 (1, 'Apple', 95, 1, 25, 0.3, 0.5, 1),
 (1, 'Egg', 78, 2, 1.1, 5.3, 6.3, 2);
-
--- Insert test foods (linked to a recipe)
-INSERT INTO Foods (recipe_id, name, calories, amount, carbs, fat, protein, position)
-VALUES
-(1, 'Chicken Breast', 165, 1, 0, 3.6, 31, 1),
-(1, 'Lettuce', 15, 1, 2.9, 0.2, 1.4, 2);
-
--- Insert test Days_Recipes
-INSERT INTO Days_Recipes (date, recipe_id, position)
-VALUES
-('2025-05-20', 1, 1),
-('2025-05-20', 2, 2);
--- run.sh
+++ run.sh
@@ -19,7 +19,8 @@ function rebuild () {
 function dev() {
 	docker compose -f docker-compose.yaml -f docker-compose.dev.yaml down
 	docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up --build -d
-	docker exec -i mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'YourStrong!Passw0rd' -d master -i /docker-entrypoint-devdb.d/dev.sql
+	#docker exec -i mssql //opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P 'YourStrong!Passw0rd' -d master -i //docker-entrypoint-initdb.d/dev.sql
+	docker exec -i mssql //opt/mssql-tools18/bin/sqlcmd -S "tcp:localhost,1433" -U sa -P 'YourStrong!Passw0rd' -d master -i //docker-entrypoint-initdb.d/dev.sql -C
 }
 
 # Starts the database
