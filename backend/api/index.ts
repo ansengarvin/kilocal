ea21b7802613c6f3d94087234d05fbc8a2a7554b
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-24 16:54:52 -0700

    fix: removed recipes required from router index

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 const router = Router();
 
 router.use("/users", require("./users"));
-router.use("/recipes", require("./recipes"));
 router.use("/days", require("./days"));
 
 module.exports = router;
