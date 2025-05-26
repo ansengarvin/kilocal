90318d6faba2eb75f9a2f8b10e57cc664bdfe7a2
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-25 18:58:20 -0700

    Revert "fix(ci/cd): send tests to 127.0.0.1 instead of localhost for ci"
    This reverts commit a323e5958df358ed6a3d7a798ac9c00b7a9b526a.

--- tests/fixtures/TestUser.ts
+++ tests/fixtures/TestUser.ts
@@ -51,7 +51,7 @@ export function createTestWithUser(
         },
         kcalApiContext: async ({ user }, use) => {
             const kcalApiContext = await playwright.request.newContext({
-                baseURL: "http://127.0.0.1:8000/",
+                baseURL: "http://localhost:8000/",
                 extraHTTPHeaders: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${user.token}`,
