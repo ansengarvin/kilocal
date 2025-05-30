2c687bfa11e499bda3c5058d2d3c9896317f1cf2
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-30 11:19:49 -0700

    wip(refactor): add async thunk for getting day

--- frontend/src/redux/journalSlice.ts
+++ frontend/src/redux/journalSlice.ts
@@ -1,31 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+import { apiURL } from "../lib/defines";
+import { firebaseAuth } from "../lib/firebase";
+
+export interface Food {
+    name: string;
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+    amount: number;
+}
 
 export interface JournalState {
     day: string;
     dayName: string;
     apiDate: string;
     isToday?: boolean;
+    totalCalories: number;
+    totalCarbs: number;
+    totalProtein: number;
+    totalFat: number;
+    food: Food[];
 }
 
-function getJournalDayName(date: Date): string {
-    const month = date.toLocaleString("default", { month: "long" });
-    const day = date.getDate();
-    const year = date.getFullYear();
-    return `${month} ${day}, ${year}`;
-}
-
-function getAPIDate(date: Date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
-    return `${year}-${month}-${day}`;
-}
+const fetchDayByDate = createAsyncThunk("journal/fetchDayByDAte", async (_, thunkAPI) => {
+    const state = thunkAPI.getState() as RootState;
+    const token = await firebaseAuth.currentUser?.getIdToken();
+    const response = await fetch(`${apiURL}/days/${state.journal.apiDate}`, {
+        method: "GET",
+        headers: {
+            Authorization: "Beader " + token,
+        },
+    });
+    return response.json();
+});
 
 const initialState: JournalState = {
     day: new Date().toISOString(),
     dayName: getJournalDayName(new Date()),
     apiDate: getAPIDate(new Date()),
+    totalCalories: 0,
+    totalCarbs: 0,
+    totalProtein: 0,
+    totalFat: 0,
+    food: [],
     isToday: true, // Default to true since the initial day is today
 };
 
@@ -51,7 +70,40 @@ export const journalSlice = createSlice({
             state.apiDate = getAPIDate(thisDay);
         },
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchDayByDate.fulfilled, (state, action) => {
+            const data = action.payload;
+            state.food = data.food;
+            state.totalCalories = data.totalCalories;
+            state.totalCarbs = data.totalCarbs;
+            state.totalProtein = data.totalProtein;
+            state.totalFat = data.totalFat;
+
+            // Update the day name and apiDate based on the fetched date
+            const fetchedDate = new Date(state.day);
+            state.dayName = getJournalDayName(fetchedDate);
+            state.apiDate = getAPIDate(fetchedDate);
+        });
+    },
 });
 
 export const { nextDay, prevDay } = journalSlice.actions;
 export default journalSlice.reducer;
+
+/*
+    Helper Functions
+*/
+function getJournalDayName(date: Date): string {
+    const month = date.toLocaleString("default", { month: "long" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+}
+
+function getAPIDate(date: Date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}-${month}-${day}`;
+}
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createAsyncThunk } from "@reduxjs/toolkit";
 import journalReducer from "./journalSlice";
+import { apiURL } from "../lib/defines";
 
 export const store = configureStore({
     reducer: {
