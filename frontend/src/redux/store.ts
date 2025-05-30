2b2056b37bef82b4ecd2d977be636acb2e44541f
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-30 11:22:00 -0700

    chore: remove unused imports from store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,6 +1,5 @@
-import { configureStore, createAsyncThunk } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import journalReducer from "./journalSlice";
-import { apiURL } from "../lib/defines";
 
 export const store = configureStore({
     reducer: {
