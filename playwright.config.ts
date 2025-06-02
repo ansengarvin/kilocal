a5e2a73400857a88040a826b58668c8c13db966c
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-06-01 18:37:27 -0700

    test: changed output location and added auto-opening html report on test fail

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,4 +3,6 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
     // Look for test files in the "tests" directory, relative to this configuration file.
     testDir: "tests",
+    outputDir: "tests/logs/playwright/results",
+    reporter: [["dot"], ["html", { open: "on-failure", outputFolder: "tests/logs/playwright/html" }]],
 });
